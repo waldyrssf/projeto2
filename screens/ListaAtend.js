663deb6dcb9e80bba24a7e18a188296240cb4093
Waldyr <97638604+waldyrssf@users.noreply.github.com>
2023-02-03 22:00:22 -0300

    Update ListaAtend.js

--- screens/ListaAtend.js
+++ screens/ListaAtend.js
@@ -1,162 +1,71 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 import {
-  Button,
-  Header,
-  ListItem,
-  Avatar,
+Button,
+Header,
+ListItem,
+Avatar,
 } from "react-native-elements";
 import { ImageBackground } from "react-native";
 import { StyleSheet } from "react-native";
 
 const list = [
-  {
-    name: "Amy Farha",
-    avatar_url:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-rKfA5hRAz7OznF1Z8flH5MjjC-J-3T4EA&usqp=CAU",
-    subtitle: "Cardiologista - USB Seattle - QUA 1/2/23 - 8h", 
-  },
-  {
-    name: "Chris Jackson",
-    avatar_url:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYPRLSISP2uoEdGxNPVFrz02gI2KWiJ_VwNA&usqp=CAU",
-    subtitle: "Dermatologista - USB Jefferson - QUI 9/2/23 - 9h",
-  },
+{
+name: "Amy Farha",
+avatar_url:
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-rKfA5hRAz7OznF1Z8flH5MjjC-J-3T4EA&usqp=CAU",
+subtitle: "Cardiologista - USB Seattle - QUA 1/2/23 - 8h",
+},
+{
+name: "Chris Jackson",
+avatar_url:
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYPRLSISP2uoEdGxNPVFrz02gI2KWiJ_VwNA&usqp=CAU",
+subtitle: "Dermatologista - USB Jefferson - QUI 9/2/23 - 9h",
+},
 ];
 
 export default function ListaAtendimento({ navigation }) {
-  return (
-    <View style={style.container}>
-      <ImageBackground
-        resizeMode="cover"
-        source={require("../assets/gradient2.png")}
-        style={style.image2}
-      />
+return (
+<View style={style.container}>
+<ImageBackground
+resizeMode="cover"
+source={require("../assets/gradient2.png")}
+style={style.image2}
+/>
 
-      <View>
-        <Header
-          leftComponent={
-            <Button title="<" onPress={() => navigation.navigate("PerfilCliente")} />
-          }
-          centerComponent={{ text: "LISTA DE MÉDICOS", style: { color: "#fff" } }}
-        />
-      </View>
 
-      <View>
-        {list.map((l, i) => (
-          <ListItem key={i} bottomDivider>
-            <Avatar source={{ uri: l.avatar_url }} />
-            <ListItem.Content>
-              <ListItem.Title>{l.name}</ListItem.Title>
-              <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-        ))}
-      </View>
-    </View>
-  );
+  <Header
+    leftComponent={
+      <Button title="<" onPress={() => navigation.navigate("PerfilCliente")} />
+    }
+    centerComponent={{ text: "LISTA DE MÉDICOS", style: { color: "#fff" } }}
+  />
+
+  <View>
+    {list.map((l, i) => (
+      <ListItem key={i} bottomDivider>
+        <Avatar source={{ uri: l.avatar_url }} />
+        <ListItem.Content>
+          <ListItem.Title>{l.name}</ListItem.Title>
+          <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    ))}
+  </View>
+</View>
+
+);
 }
 
-const button1 = {
-  Button: {
-    buttonStyle: {
-      backgroundColor: "#6865F7",
-      borderRadius: 30,
-
-      width: "100%",
-      marginVertical: 10,
-      paddingVertical: 25,
-      paddingHorizontal: 82,
-
-      shadowColor: "#171717",
-      shadowOffset: { width: -1, height: 2.5 },
-      shadowOpacity: 0.2,
-      shadowRadius: 3,
-    },
-    buttonText: {
-      fontSize: 18,
-      fontFamily: "Gill Sans",
-      textAlign: "center",
-      margin: 10,
-      color: "#ffffff",
-      backgroundColor: "transparent",
-    },
-  },
-};
-const button2 = {
-  Button: {
-    buttonStyle: {
-      backgroundColor: "#6865F7",
-      borderRadius: 30,
-      width: "100%",
-
-      marginVertical: 10,
-      paddingVertical: 25,
-      paddingHorizontal: 70,
-
-      shadowColor: "#171717",
-      shadowOffset: { width: -1, height: 2.5 },
-      shadowOpacity: 0.2,
-      shadowRadius: 3,
-    },
-    buttonText: {
-      fontSize: 18,
-      fontFamily: "Gill Sans",
-      textAlign: "center",
-      margin: 10,
-      color: "#ffffff",
-      backgroundColor: "transparent",
-    },
-  },
-};
-const Vbutton = {
-  container: {
-    backgroundColor: "#ffff",
-    borderRadius: 30,
-    width: "100%",
-    margin: 1,
-    paddingVertical: 60,
-    paddingHorizontal: 60,
-    justifyContent: "center",
-  },
-  shadowProp: {
-    shadowColor: "#171717",
-    shadowOffset: { width: 0, height: 6 },
-    shadowOpacity: 0.15,
-    shadowRadius: 3,
-  },
-  text: {
-    fontSize: 20,
-    fontFamily: "corbel",
-    textAlign: "center",
-    justifyContent: "center",
-  },
-};
 const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-  },
-  image: {
-    position: "absolute",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "60%",
-    height: 133,
-    marginLeft: 40,
-    marginRight: 40,
-    marginTop: 20,
-    marginBottom: 20,
-  },
-
-  image2: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    paddingVertical: 350,
-    position: "relative",
-  },
+container: {
+flex: 1,
+flexDirection: "column",
+justifyContent: "center",
+},
+image2: {
+position: "absolute",
+width: "100%",
+height: "100%",
+},
 });
